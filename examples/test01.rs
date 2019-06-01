cdb35e1ffe874f8b2cbdee428f07832dad34b3e5
Matthias Geier <Matthias.Geier@gmail.com>
2019-06-01 13:47:53 +0200

    Add example

---
+++ examples/test01.rs
@@ -0,0 +1,56 @@
+use std::fs;
+
+use failure::Error;
+
+use disk_streaming::file::{AudioFile, Block, ProvideBlocks, converter::Converter};
+
+fn main() -> Result<(), Error> {
+    //let file = fs::File::open("ukewave.ogg")?;
+    let file = fs::File::open("marimba.ogg")?;
+    let mut af = AudioFile::new(file)?;
+
+    println!("samplerate: {}", af.samplerate());
+    println!("channels: {}", af.channels());
+
+    let mut buffer = Vec::with_capacity(af.len());
+    println!("capacity: {}", buffer.capacity());
+
+    assert!(af.channels() > 0);
+
+    if let AudioFile::Vorbis(ref mut f) = af {
+        loop {
+            let block = f.next_block(1024)?;
+            if block.len() == 0 {
+                break;
+            }
+            buffer.extend(&mut block.channel_iterators()[0]);
+        }
+    }
+
+    println!("total buffer size: {}", buffer.len());
+
+    println!("===========");
+
+    buffer.clear();
+    af.seek(0)?;
+
+    let mut conv = Converter::new(af, 48_000)?;
+
+    println!("target samplerate: {}", conv.samplerate());
+    println!("target channels: {}", conv.channels());
+    assert!(conv.channels() > 0);
+
+    println!("target length:     {}", conv.len());
+
+    loop {
+        let block = conv.next_block(1024)?;
+        if block.len() == 0 {
+            break;
+        }
+        buffer.extend(&mut block.channel_iterators()[0]);
+    }
+
+    println!("total buffer size: {}", buffer.len());
+
+    Ok(())
+}
